5623a43665f1f2239b67dce52491e1a968551bd0
joaoVictorS <jonnymcz2@gmail.com>
2023-04-13 01:23:40 -0300

    Creating the app core and adding its models entities

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,5 +9,6 @@ router.register(r'groups', GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
